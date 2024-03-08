188ca7da50b83fb4c11e19a0a1a4e24812e5ae2f
Diveturtle93 <Hafemeister1@hotmail.de>
2024-03-08 10:30:37 +0100

    Erweitern der Temperatur Messung
    - Multiplexer Daten werden nun nicht nur an das erste Device in der LTC6811 Chain übergeben
    - Datenarray wird multipliziert mit LTC6811 Devices und an alle gesendet

--- Application/Src/LTC1380.c
+++ Application/Src/LTC1380.c
@@ -44,12 +44,25 @@ void ltc1380_write(uint8_t Address, uint8_t Channel)
 	// Definiere Array fuer Channel auswaehlen
 	uint8_t off[6] = {0x69, 0x08, 0x00, 0x89, 0x7F, 0xF9};					// Daten-Bytes fuer LTC6811 mit Daten fuer LTC1380
 	
+	// Definiere Arra fuer LTC6811 Chain
+	uint8_t data[LTC6811_DEVICES * 6] = {0};								// Datenarray fuer LTC6811 Chain
+
 	// Addresse und Channel waehlen
 	off[1] |= ((Address & 0x0F) << 4);										// Address in Array uebertragen
 	off[3] |= ((Channel & 0x0F) << 4);										// Channel in Array uebertragen
 	
+	// LTC1380 Daten in Datenarray fuer LTC6811 schreiben
+	for (uint8_t j = 0; j < LTC6811_DEVICES; j++)
+	{
+		// Fuer jeden LTC6811 wiederholen
+		for (uint8_t i = 0; i < 6; i++)
+		{
+			data[j * i] = off[i];											// Jeder LTC6811 bekommt die selben Daten
+		}
+	}
+
 	// LTC6811 beschreiben und ueber I2C uebertragen
-	ltc6811_write(WRCOMM, &off[0]);											// Daten in LTC6811 schreiben
+	ltc6811_write(WRCOMM, &data[0]);										// Daten in LTC6811 schreiben
 	ltc6811(STCOMM);														// Daten von LTC6811 an LTC1380 senden
 
 	// Debug Nachricht
@@ -82,11 +95,24 @@ void ltc1380_off(uint8_t Address)
 	// Definiere Array fuer alle Kanaele aus
 	uint8_t off[6] = {0x69, 0x08, 0x00, 0x09, 0x7F, 0xF9};					// Daten-Bytes fuer LTC6811 mit Daten fuer LTC1380
 	
+	// Definiere Arra fuer LTC6811 Chain
+	uint8_t data[LTC6811_DEVICES * 6] = {0};								// Datenarray fuer LTC6811 Chain
+
 	// Addresse waehlen
 	off[1] |= ((Address & 0x0F) << 4);										// Address in Array uebertragen
 	
+	// LTC1380 Daten in Datenarray fuer LTC6811 schreiben
+	for (uint8_t j = 0; j < LTC6811_DEVICES; j++)
+	{
+		// Fuer jeden LTC6811 wiederholen
+		for (uint8_t i = 0; i < 6; i++)
+		{
+			data[j * i] = off[i];											// Jeder LTC6811 bekommt die selben Daten
+		}
+	}
+
 	// LTC6811 beschreiben und ueber I2C uebertragen
-	ltc6811_write(WRCOMM, &off[0]);											// Daten in LTC6811 schreiben
+	ltc6811_write(WRCOMM, &data[0]);											// Daten in LTC6811 schreiben
 	ltc6811(STCOMM);														// Daten von LTC6811 an LTC1380 senden
 
 	// Debug Nachricht
@@ -118,14 +144,27 @@ void ltc1380_alloff(void)
 	// Definiere Array fuer alle Multiplexer aus
 	uint8_t off[6] = {0x69, 0x08, 0x00, 0x09, 0x7F, 0xF9};					// Daten-Bytes fuer LTC6811 mit Daten fuer LTC1380
 	
+	// Definiere Arra fuer LTC6811 Chain
+	uint8_t data[LTC6811_DEVICES * 6] = {0};								// Datenarray fuer LTC6811 Chain
+
 	// Schleife um Daten an LTC6811 zu uebertragen
 	for (uint8_t i = 0; i < LTC1380_DEVICES; i++)
 	{
 		// Addresse waehlen
 		off[1] |= (((i + 1) & 0x0F) << 4);									// Address in Array uebertragen
 		
+		// LTC1380 Daten in Datenarray fuer LTC6811 schreiben
+		for (uint8_t j = 0; j < LTC6811_DEVICES; j++)
+		{
+			// Fuer jeden LTC6811 wiederholen
+			for (uint8_t i = 0; i < 6; i++)
+			{
+				data[j * i] = off[i];										// Jeder LTC6811 bekommt die selben Daten
+			}
+		}
+
 		// LTC6811 beschreiben und ueber I2C uebertragen
-		ltc6811_write(WRCOMM, &off[0]);										// Daten in LTC6811 schreiben
+		ltc6811_write(WRCOMM, &data[0]);										// Daten in LTC6811 schreiben
 		ltc6811(STCOMM);													// Daten von LTC6811 an LTC1380 senden
 
 		// Debug Nachricht
